5f556e370410d7f7f9509754ce4de0e50e6ffecf
guitit <guitit123@gmail.com>
2021-04-29 11:44:41 +0300

    first

---
+++ getparams.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>your product</title>
+    </head>
+    <body>
+    <?php 
+    $productname=$_GET["productname"];
+    $productcolor=$_GET["productcolor"];
+    echo"<p>your product name is:".$prodactname."</p>"."<p>your product color is:".$productcolor."</p>";
+
+    ?>
+    </body>
+</html>
